5259be5556ebf212fd4126e07cbc52e558ad48fa
Yash Mittal <yashmittalmm@gmail.com>
2025-05-16 12:04:28 +0530

    chore: Update environment file

--- environment.d.ts
+++ environment.d.ts
@@ -1,10 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NEXT_PUBLIC_SUPABASE_URL: string
+      NEXT_PUBLIC_TESTNET_NODE_URL: string
       NODE_ENV: 'development' | 'production'
-      SUPABASE_PROJECT_REF: string
-      SUPABASE_SERVICE_ROLE_KEY: string
     }
   }
 }
